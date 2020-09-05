f8238652f3c935ee436c0c551dce04c2cb57faab
idpapaga <a.krotenko@gitlab.su>
2020-09-05 14:49:16 +0500

    page list

--- src/containers/TestingList/TestingList.js
+++ src/containers/TestingList/TestingList.js
@@ -1,10 +1,31 @@
-import React, {Component} from 'react'
-// import classes from './TestingList.module.css'
+import React, { Component } from 'react'
+import classes from './TestingList.module.css'
+import { NavLink } from 'react-router-dom'
 
 class TestingList extends Component {
+    renderTesting() {
+        return [1, 2, 3].map((testing, index) => {
+            return (
+                <li key={index}>
+                    <NavLink to={'/testing/' + testing}>
+                        Test number {testing}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <h1>Testing List</h1>
+            <div className={classes.TestingList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderTesting()}  
+                    </ul>
+                </div>
+            </div>
         )
     }
 }

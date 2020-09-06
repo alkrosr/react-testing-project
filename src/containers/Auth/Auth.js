bb1c5ba8db7da7f68c9938ff36b8bd57891bb087
idpapaga <a.krotenko@gitlab.su>
2020-09-06 07:00:42 +0500

    textbox component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 class Auth extends Component {
     loginHandler = () => {
@@ -22,8 +23,15 @@ class Auth extends Component {
                     <h1>Авторизация</h1>
 
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                        <input type="text" />
-                        <input type="text" />
+
+                        <Input
+                            label='E-mail'
+
+                        />
+                        <Input
+                            label='Password'
+                            errorMessage={'TEST'}
+                        />
 
                         <Button
                             type='success'

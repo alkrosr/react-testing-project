ddde52439542a4e733445c07c35f72d20d248043
idpapaga <a.krotenko@gitlab.su>
2020-09-07 21:03:45 +0500

    create text

--- src/containers/TestingCreator/TestingCreator.js
+++ src/containers/TestingCreator/TestingCreator.js
@@ -1,10 +1,54 @@
-import React, {Component} from 'react'
-// import classes from './TestingCreator.module.css'
+import React, { Component } from 'react'
+import classes from './TestingCreator.module.css'
+import Button from '../../components/UI/Button/Button'
 
 class TestingCreator extends Component {
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+    addQuestionHandler = () => {
+
+    }
+
+    createTestingHandler = () => {
+
+    }
+
     render() {
         return (
-            <h1>Testing Creator</h1>
+            <div className={classes.TestingCreator}>
+                <div>
+                    <h1>Создание теста</h1>
+
+                    <form onSubmit={this.submitHandler}>
+
+                        <input type='text' />
+                        <hr />
+                        <input type='text' />
+                        <input type='text' />
+                        <input type='text' />
+                        <input type='text' />
+
+
+                        <select></select>
+
+                        <Button
+                            type='primary'
+                            onClick={this.addQuestionHandler}
+                        >
+                            Добавить вопрос
+                        </Button>
+                        <Button
+                            type='success'
+                            onClick={this.createTestingHandler}
+                        >
+                            Создать тест
+                        </Button>
+
+                    </form>
+                </div>
+            </div>
         )
     }
 }

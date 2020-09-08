a426200a78380a0570c13c338e14bee466ad96e8
idpapaga <a.krotenko@gitlab.su>
2020-09-08 18:16:20 +0500

    changing controls

--- src/containers/TestingCreator/TestingCreator.js
+++ src/containers/TestingCreator/TestingCreator.js
@@ -3,7 +3,7 @@ import classes from './TestingCreator.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import Select from '../../components/UI/Select/Select'
-import { createControl } from '../../form/formFramework'
+import { createControl, validate, validateForm } from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 
 function createOptionControl(number) {
@@ -30,6 +30,7 @@ function createFormControls() {
 class TestingCreator extends Component {
     state = {
         testing: [],
+        isFormValid: false,
         rightAnswerId: 1,
         formControls: createFormControls()
     }
@@ -38,36 +39,48 @@ class TestingCreator extends Component {
         event.preventDefault()
     }
 
-    addQuestionHandler = () => {
-
+    addQuestionHandler = (event) => {
+        event.preventDefault()
     }
 
     createTestingHandler = () => {
 
     }
 
-    controlName = (value, controlName) => {
+    changeHandler = (value, controlName) => {
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
 
+        control.touched = true
+        control.value = value
+        control.valid = validate(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        this.setState({
+            formControls,
+            isFormValid: validateForm(formControls)
+        })
     }
 
     renderControls() {
         return Object.keys(this.state.formControls).map((controlName, index) => {
             const control = this.state.formControls[controlName]
-
+ 
             return (
                 <Auxiliary key={controlName + index}>
-                    <Input
-                        label={control.label}
-                        value={control.value}
-                        valid={control.valid}
-                        shouldValidation={!!control.validation}
-                        touched={control.touched}
-                        errorMessage={control.errorMessage}
-                        onChange={event => this.changeHandler(event.target.value, controlName)}
-                    />
-                    {index === 0 ? <hr /> : null}
+                  <Input
+                    label={control.label}
+                    value={control.value}
+                    valid={control.valid}
+                    shouldValidate={!!control.validation}
+                    touched={control.touched}
+                    errorMessage={control.errorMessage}
+                    onChange={event => this.changeHandler(event.target.value, controlName)}
+                  />
+                  { index === 0 ? <hr /> : null }
                 </Auxiliary>
-            )
+              )
         })
     }
 
@@ -102,12 +115,14 @@ class TestingCreator extends Component {
                         <Button
                             type='primary'
                             onClick={this.addQuestionHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Добавить вопрос
                         </Button>
                         <Button
                             type='success'
                             onClick={this.createTestingHandler}
+                            disabled={this.state.testing.length === 0}
                         >
                             Создать тест
                         </Button>

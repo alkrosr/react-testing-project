775c91214890122bdb2ef8b744a1d6c53cb47595
idpapaga <a.krotenko@gitlab.su>
2020-09-08 23:09:22 +0500

    loading test list

--- src/containers/TestingList/TestingList.js
+++ src/containers/TestingList/TestingList.js
@@ -4,23 +4,48 @@ import { NavLink } from 'react-router-dom'
 import axios from 'axios'
 
 class TestingList extends Component {
+
+    state = {
+        testinges: []
+    }
+
+
     renderTesting() {
-        return [1, 2, 3].map((testing, index) => {
+        return this.state.testinges.map((testing) => {
             return (
-                <li key={index}>
-                    <NavLink to={'/testing/' + testing}>
-                        Test number {testing}
+                <li key={testing.id}>
+                    <NavLink to={'/testing/' + testing.id}>
+                        {testing.name}
                     </NavLink>
                 </li>
             )
         })
     }
 
-    componentDidMount() {
-        axios.get('https://react-testing-d6141.firebaseio.com/testing.json').then(response => {
-            console.log(response)
-        })
+    async componentDidMount() {
+        try {
+            const response = await axios.get('https://react-testing-d6141.firebaseio.com/testinges.json')
+            const testinges = []
+            Object.keys(response.data).forEach((key, index) => {
+                testinges.push({
+                    id: key,
+                    name: `Тест №${index + 1}`
+                })
+            })
+
+            this.setState({
+                testinges
+            })
+        } catch (e) {
+            console.log(e)
+        }
     }
+
+    // componentDidMount() {
+    //     axios.get('https://react-testing-d6141.firebaseio.com/testing.json').then(response => {
+    //         console.log(response)
+    //     })
+    // }
 
     render() {
         return (
@@ -29,7 +54,7 @@ class TestingList extends Component {
                     <h1>Список тестов</h1>
 
                     <ul>
-                        {this.renderTesting()}  
+                        {this.renderTesting()}
                     </ul>
                 </div>
             </div>
